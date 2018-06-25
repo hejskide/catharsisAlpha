bb5400343935124f38d5f9bd45e62761f4df9a84
hejskide <luismartinez110796@gmail.com>
2018-06-25 10:28:14 -0400

    Primeros assets

---
+++ assets/scripts/controller.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DIRECCION { FRONT, BACK , LEFT , RIGHT}
+
+public class controller : MonoBehaviour
+{
+    DIRECCION dir;
+    Vector3 pos;
+    int angle;
+    public float speed = 2;
+
+
+    private void Start()
+    {
+        dir = DIRECCION.FRONT;
+    }
+
+    void Update()
+    {
+
+        Vector3 pos = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical"));
+        if (pos != Vector3.zero)
+        {
+            if (Mathf.Abs(pos.x) > Mathf.Abs(pos.z))
+            {
+                if (pos.x > 0)
+                {
+                    angle = 90;
+                }
+                else
+                {
+                    angle = 270;
+                }
+            }
+            else if (Mathf.Abs(pos.x) == Mathf.Abs(pos.z)) {
+
+                if (pos.x > 0 && pos.z > 0)
+                {
+                    angle = 45;
+                }
+                else if (pos.x < 0 && pos.z > 0)
+                {
+                    angle = 315;
+                }
+                else if (pos.x > 0 && pos.z < 0)
+                {
+                    angle = 135;
+                }
+                else
+                {
+                    angle = 225;
+                }
+            }
+            else
+            {
+                if (pos.z > 0)
+                {
+                    angle = 360;
+                }
+                else
+                {
+                    angle = 180;
+                }
+            }
+
+        }
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.up);
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + pos, speed * Time.deltaTime);
+
+    }
+
+    void MoverPersonaje()
+    {
+
+        /*if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            angle = 0;
+            pos += Vector3.forward;
+            //Debug.Log("arriba");
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            angle = 180;
+            pos += Vector3.back;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            angle = 270;
+            pos += Vector3.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            angle = 90;
+            pos += Vector3.right;
+        }*/
+
+        
+
+        /*if (targetDirection != Vector3.zero)
+        {
+            if (Mathf.Abs(targetDirection.y) > Mathf.Abs(targetDirection.z))
+            {
+                if (targetDirection.y > 0)
+                {
+                    pos += Vector3.right;
+                }
+                else
+                {
+                    pos += Vector3.left;
+                }
+            }
+            else
+            {
+                if (targetDirection.z > 0)
+                {
+                    pos += Vector3.forward;
+                }
+                else
+                {
+                    pos += Vector3.back;
+                }
+            }
+
+        }*/
+
+    }
+    
+}
---
+++ assets/scripts/raycast.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raycast : MonoBehaviour {
+
+    /*public float raydist = 10;
+
+    private void Update()
+    {
+        RaycastHit hit;
+        float dist;
+
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * raydist;
+        Debug.DrawRay(transform.position, forward, Color.green);
+
+        if (Physics.Raycast(transform.position, (forward), out hit))
+        {
+            dist = hit.distance;
+            print(dist + " " + hit.collider.gameObject.name);
+        }
+    }*/
+
+    /*public float maxRay = 1f;
+
+    private void FixedUpdate()
+    {
+        Ray ray = new Ray(transform.position, Vector3.forward);
+        RaycastHit hit;
+
+        Debug.DrawLine(transform.position, transform.position + Vector3.forward * maxRay, Color.red);
+
+        if(Physics.Raycast(ray, out hit, maxRay))
+        {
+            Debug.Log("you hit a ray");
+        }
+    }*/
+
+    private void Update()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.forward);
+        Debug.DrawRay(ray.origin, ray.direction, Color.red);
+
+        if(Physics.Raycast(ray, out hit, 1f))
+        {
+            Debug.Log("abrir " + hit.transform.name + "?");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Debug.Log(hit.transform.name);
+            Debug.Log("abriste " + hit.transform.name);
+        }
+    }
+}
