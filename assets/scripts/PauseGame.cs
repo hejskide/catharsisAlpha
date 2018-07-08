606b3430e861c32976db7711c56e75cb1c2ee6a6
oilujc <jcmartinezvalero@gmail.com>
2018-07-08 14:59:30 -0400

    Pause Scripts and UI

---
+++ assets/scripts/PauseGame.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class PauseGame : MonoBehaviour
+{
+
+	public GameObject pauseMenuUI;
+	public GameObject buttonPause;
+	public static bool gameIsPaused = false;
+
+	// Update is called once per frame
+	void Update ()
+	{	
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if (gameIsPaused) {
+				Resume ();
+			} else {
+				Pause ();
+			}
+		}
+	}
+
+	public void Resume ()
+	{
+		pauseMenuUI.SetActive (false);
+		buttonPause.SetActive (true);
+		Time.timeScale = 1;
+		gameIsPaused = false;
+	}
+
+	public void Pause ()
+	{
+		pauseMenuUI.SetActive (true);
+		buttonPause.SetActive (false);
+		Time.timeScale = 0;
+		gameIsPaused = true;
+	}
+
+
+		
+}
